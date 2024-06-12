53da74dd725bc25e7387d700a39de028f4dd6a20
HaeToon <gxg4205@gmail.com>
2024-06-12 15:13:01 +0900

    xml fix , notice fixed

--- src/main/java/com/hrproject/hrproject/dto/NoticeDto.java
+++ src/main/java/com/hrproject/hrproject/dto/NoticeDto.java
@@ -1,11 +1,19 @@
 package com.hrproject.hrproject.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class NoticeDto {
-    private int no;
+    private int empno;
+    private int notice_no;
     private String writer;
     private String subject;
     private String content;
-    private String img;
     private String regdate;
     private int hit;
 }
